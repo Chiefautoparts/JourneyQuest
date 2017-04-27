125427f7257c80d31d0feafa54edca5aea5db7f9
Chiefautoparts <chiefautoparts@outlook.com>
2017-04-27 03:39:10 -0700

    started on the JS

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+// var fighter = []
+// var characters = [fighter, mage, archer];
+//
+// var character = {
+//   chClass: 'Fighter',
+//   health: 100,
+//   magika: 25,
+//   weapon: 'two-handed sword',
+//   special: 'Power attack x2 damage',
+//   displayStats: function() {
+//     return('Fighter: 100HP 25MG');
+//   }
+// };
+var play = document.getElementsByTagName('')
+function startQuest() {
+  var sceneOne = document.createElement('section')
+}
