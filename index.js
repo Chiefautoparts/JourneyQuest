1f7bcd8d2552a680a49eea2ea3ebd6f805745725
Chiefautoparts <chiefautoparts@outlook.com>
2017-04-28 04:27:19 -0700

    working with DOM

--- index.js
+++ index.js
@@ -12,7 +12,11 @@
 //     return('Fighter: 100HP 25MG');
 //   }
 // };
-var play = document.getElementsByTagName('')
-function startQuest() {
-  var sceneOne = document.createElement('section')
-}
+var begining = 'You awake in a haze in a room you have never been in before. Confused, your mind in a haze you quickly take in your surroundings. To your left there is a door leaving the room, straight in front of you are several bookselves and a desk with scattered pappers across it and to your right there is wardrobe what appears to be a sleeping area and several cabinets one of which is locked.';
+
+var startQuest = document.getElementById('scene');
+var playerChoices = document.getElementById('choices'); //ordered list id//
+var optionOne = document.getElementById('one');
+var optionTwo = document.getElementById('two');
+var optionThree = document.getElementById('three');
+startQuest.innerText = begining;
