d98a7e8b9e6c5044e3ac6ad02e0f1d85bbddfad9
Chiefautoparts <chiefautoparts@outlook.com>
2017-04-29 10:31:47 -0700

    working on DOM

--- index.js
+++ index.js
@@ -12,17 +12,21 @@
 //     return('Fighter: 100HP 25MG');
 //   }
 // };
-var begining = 'You awake in a haze in a room you have never been in before. Confused, your mind in a haze you quickly take in your surroundings. To your left there is a door leaving the room, straight in front of you are several bookselves and a desk with scattered pappers across it and to your right there is wardrobe what appears to be a sleeping area and several cabinets one of which is locked. Do you....';
+var starting = 'You awake in a haze in a room you have never been in before. Confused, your mind in a haze you quickly take in your surroundings. To your left there is a door leaving the room, straight in front of you are several bookselves and a desk with scattered pappers across it and to your right there is wardrobe what appears to be a sleeping area and several cabinets one of which is locked. Do you....';
+var createGame = function() {
+  var game = document.getElementsByTagName('ol')[0];
 
-var startQuest = document.getElementById('scene');
-var playerChoices = document.getElementById('choices'); //ordered list id//
-var optionOne = document.getElementById('one');
-var optionTwo = document.getElementById('two');
-var optionThree = document.getElementById('three');
-startQuest.innerText = begining;
-optionOne.innerText = ' Search the room?';
-optionTwo.innerText = ' Get up and immediately exit through the door?';
-optionThree.innerText = 'Begin to panic, become aggressive and start smashing everything in sight. While screaming of course';
-playerChoices.appendChild(optionOne);
-playerChoices.appendChild(optionTwo);
-playerChoices.appendChild(optionThree);
+  var optionOne = document.createElement('li');
+  var textOne = document.createTextNode('Search the room.');
+  optionOne.appendChild(textOne);
+  game.appendChild(optionOne);
+};
+// var optionTwo = document.getElementById('two');
+// var optionThree = document.getElementById('three');
+//
+// optionTwo.innerText = ' Get up and immediately exit through the door?';
+// optionThree.innerText = 'Begin to panic, become aggressive and start smashing everything in sight. While screaming of course';
+// ;
+// playerChoices.appendChild(optionTwo);
+// playerChoices.appendChild(optionThree);
+createGame();
