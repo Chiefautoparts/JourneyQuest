b55e67ca884c44eca11b53c6ba9a713f81e1f7db
Chiefautoparts <chiefautoparts@outlook.com>
2017-05-18 03:35:08 -0700

    started writing second prompt

--- 2ndGO/app.js
+++ 2ndGO/app.js
@@ -2,6 +2,10 @@
 
 var userName = prompt('What is your name?');
 console.log('welcome ' + userName + '. Now let us begin the Journey.');
+// var reset = prompt('Are you ready to continue ' + userName + '? (please enter yes or no into the input field)');
+// if(reset = yes){
+//
+// }
 function createGame(){
   function firstEvent(){
     var One = prompt('You awake in a room, there are several chests around, a desk, a couple book shelves and a bed. (please enter 1, 2, or   3 for you choice.) 1. exit the room quickly 2. search the room 3. panic and begin smashing everything you can get your hands on.');
@@ -12,9 +16,9 @@ function createGame(){
     } else {
       console.log('You start screaming as you run around the room, pulling the bookshelves to the ground, you perform a perfectly exicuted  flying elbow to one of the chest smashing it open and destroying the contents inside. finally you body slam the desk breaking it in   half. observing the destruction you have just caused you are proud of it but notice that you have destryed several items that probably  would have been usual but you dont mind because you are to fucking badass (you earn secret badass points)');
     }
+    var two = prompt('Now that you are standing in the hallway you must choose do you go right or left?');
+    if(two = 'right'){
+      console.log('You decide to to head to the right. You begin walking toward the window, there is a door at the end of the hallway by the window. You here muffled voices coming from behind the door, you are able to determine there could be 2 maybe 3 people in the room.');
+    }
   }
-  firstEvent();
-
-  function secondEvent(){
-  }
-}
+};
